b25d2f28c100e395b1e54a6a1f242adf1f4f1425
Mragank Jaitly <84132608+mragankjaitly@users.noreply.github.com>
2025-01-10 05:53:21 +0530

    update

---
+++ contact.js
@@ -0,0 +1,5 @@
+function flipCard() {
+    const container = document.querySelector('.flip-container');
+    container.classList.toggle('flipped'); // Toggles the flipped class to trigger the flip effect
+  }
+  
